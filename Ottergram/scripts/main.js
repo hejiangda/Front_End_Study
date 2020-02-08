abfc2e66f4b324159ba68f0f232b02a63381d424
hejiangda <hejiangda@163.com>
2020-02-08 21:53:06 +0800

    通过向body添加class实现隐藏内容展示页

--- Ottergram/scripts/main.js
+++ Ottergram/scripts/main.js
@@ -1,6 +1,8 @@
 var DETAIL_IMAGE_SELECTOR='[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR='[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR='[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS='hidden-detail';
+var ESC_KEY=27;
 
 function setDetails(imageUrl,titleText){
     'use strict';
@@ -39,11 +41,27 @@ function getThumbnailsArray(){
     var thumbnailArray=[].slice.call(thumbnails);
     return thumbnailArray;
 }
+function hideDetails(){
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+
+}
+function addKeyPressHandler(){
+    'use strict';
+    document.body.addEventListener('keyup',function(event){
+        event.preventDefault();
+        console.log(event.keyCode);
+        if(event.keyCode==ESC_KEY){
+            hideDetails();
+        }
+    })
+}
 function initializeEvents(){
     'use strict';
     var thumbnails=getThumbnailsArray();
     thumbnails.forEach(addThumbClickHandler);
-
+    addKeyPressHandler();
 }
 
 initializeEvents();
+
