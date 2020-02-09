9a3d683d174edbbbda7ad8c2299e0ddf61b1f8a4
hejiangda <hejiangda@163.com>
2020-02-09 16:25:25 +0800

    DataStore模块ok

--- Coffeerun/scripts/datastore.js
+++ Coffeerun/scripts/datastore.js
@@ -10,7 +10,19 @@
     DataStore.prototype.add=function(key,val){
         this.data[key]=val;
     };
-    
+
+    DataStore.prototype.get=function(key){
+        return this.data[key];
+    };
+
+    DataStore.prototype.getAll=function(){
+        return this.data;
+    }
+
+    DataStore.prototype.remove=function(key){
+        delete this.data[key];
+    }
+
     App.DataStore=DataStore;
     window.App=App;
 })(window);
