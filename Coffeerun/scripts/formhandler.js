1581382d606f5252d1d7f43e983d93a612465f8c
hejiangda <hejiangda@163.com>
2020-02-14 21:50:59 +0800

    获取表单信息

--- Coffeerun/scripts/formhandler.js
+++ Coffeerun/scripts/formhandler.js
@@ -11,6 +11,19 @@
             throw new Error('Could not find element with selector:'+selector);
         }
     }
+    FormHandler.prototype.addSubmitHandler=function(){
+        console.log('Setting submit handler for form');
+        this.$formElement.on('submit',function(event){
+            event.preventDefault();
+
+            var data={};
+            $(this).serializeArray().forEach(function(item){
+                data[item.name]=item.value;
+                console.log(item.name+' is '+item.value);
+            });
+            console.log(data);
+        });
+    }
 
     App.FormHandler=FormHandler;
     window.App=App;
