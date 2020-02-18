6ef83ad85ef2cf2445369d9f23b74c8a69c46d66
hejiangda <hejiangda@163.com>
2020-02-18 13:59:12 +0800

    Delivering an Order by Clicking a Row

--- Coffeerun/scripts/checklist.js
+++ Coffeerun/scripts/checklist.js
@@ -2,7 +2,7 @@
     'use strict';
     var App=window.App||{};
     var $=window.jQuery;
-    
+
     function CheckList(selector){
         if(!selector){
             throw new Error('No selector provided');
@@ -14,11 +14,26 @@
         }
     }
 
+    CheckList.prototype.addClickHandler=function (fn){
+        this.$element.on('click','input',function(event){
+            var email=event.target.value;
+            this.removeRow(email);
+            fn(email);
+        }.bind(this));
+    };
+
     CheckList.prototype.addRow=function(coffeeOrder){
+        this.removeRow(coffeeOrder.emailAddress);
         var rowElement=new Row(coffeeOrder);
 
         this.$element.append(rowElement.$element);
     }
+
+    CheckList.prototype.removeRow=function(email){
+        this.$element.find('[value="'+email+'"]')
+        .closest('[data-coffee-order="checkbox"]')
+        .remove();
+    };
 
     function Row(coffeeOrder){
         var $div=$('<div></div>',{
--- Coffeerun/scripts/main.js
+++ Coffeerun/scripts/main.js
@@ -7,9 +7,11 @@
     var DataStore=App.DataStore;
     var FormHandler=App.FormHandler;
     var CheckList=App.CheckList;
+    
     var myTruck=new Truck('ncc-1701',new DataStore());
     window.myTruck=myTruck;
     var checkList=new CheckList(CHECKLIST_SELECTOR);
+    checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
     var formHandler=new FormHandler(FORM_SELECTOR);
 
     formHandler.addSubmitHandler(
